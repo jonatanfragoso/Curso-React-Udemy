cee4734ba2a8e7d28d11637510724a78b25f4aa3
Jonatan Fragoso <jonatanfragoso@gmail.com>
2023-12-27 11:36:03 -0500

    att. (aula de hooks Pendente)

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -0,0 +1,42 @@
+const Events = () => {
+    const handleMyEvent = (e) => {
+        // console.log(e)
+        console.log("Ativou o evento!");
+    }
+
+    const renderSomething = (x) => {
+        if(x){
+            return <h1>Renderizando isso!</h1>
+        }else {
+            return <h1>Também posso renderizar isso!</h1>
+        }
+        
+    }
+
+    return (
+        <div>
+            <div>
+                <button onClick={handleMyEvent}>Clique Aqui!</button>
+            </div>
+            <div>
+                <button onClick={() => console.log('Clicou!')}>Clique aqui também!</button>
+            </div>
+            <div>
+                <button onClick={()=> {
+                    if(true){
+                        console.log('Isso não deveria existir.')
+                    }
+                }}>
+                    clica aqui, por favor.
+                </button>
+            </div>
+            <div>
+                {renderSomething(true)}
+                {renderSomething(false)}
+            </div>
+        </div>
+    )
+
+}
+
+export default Events
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.jsx
@@ -0,0 +1,10 @@
+const TemplateExpressions = () => {
+    const name = "Jonatan"
+    return (
+        <div>
+            <h1>Olá {name}, tudo bem?</h1>
+        </div>
+    )
+}
+
+export default TemplateExpressions
